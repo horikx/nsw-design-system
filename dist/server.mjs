2fb781af18609414b199a5a6ba2721ea682c40f4
Chris Horikx <chorikx@gmail.com>
2021-03-29 14:46:38 +1100

    Dist update

---
+++ dist/server.mjs
@@ -0,0 +1,24 @@
+// // import * as handlebarsRuntime from 'handlebars/runtime'
+import handlebars from 'handlebars/runtime'
+// // import helpers from 'handlebars-helpers'
+// // import 'handlebars/runtime.js'
+// // handlebars.registerHelper(helpers)
+
+import card from './components/card/_card.hbs'
+import gwicNav from './components/gwic-nav/_gwic-nav.hbs'
+import gwicHeader from './components/gwic-header/_gwic-header.hbs'
+import gwicFooter from './components/gwic-footer/_gwic-footer.hbs'
+import gwicSubNav from './components/gwic-sub-nav/_gwic-sub-nav.hbs'
+import breadcrumbs from './components/breadcrumbs/_breadcrumbs.hbs'
+
+const templates = {
+  handlebars,
+  card,
+  breadcrumbs,
+  gwicNav,
+  gwicHeader,
+  gwicFooter,
+  gwicSubNav,
+}
+
+export default templates
